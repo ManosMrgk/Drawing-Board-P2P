a620ffe75987edef2573d6949073ef93005f0c79
Manolis Maragkoudakis <manolis.marag@gmail.com>
2020-10-15 00:37:12 +0300

    Better peer server integration

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+var options = {
+    debug: false
+}
 const io = require('socket.io')(server)
 const { v4:uuidV4 } = require('uuid')
 const port = process.env.PORT || 3000;
 console.log('listening on port',port)
 app.set('view engine','ejs')
 app.use(express.static('.'))
-
+app.use('/peerjs', ExpressPeerServer(server, options));
 app.get('/', (req,res) =>{
     res.redirect(`/${uuidV4()}`)
 })
