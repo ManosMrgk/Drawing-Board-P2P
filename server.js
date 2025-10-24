d64e3223a327c186e83370b8f4a0bee903a9406f
Manolis Maragkoudakis <manolis.marag@gmail.com>
2025-10-24 12:46:37 +0300

    Added gitignore, fixed dependencies for new deployment now that glitch closed down

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-var ExpressPeerServer = require('peer').ExpressPeerServer;
-var options = {
-    debug: false
-}
-const io = require('socket.io')(server)
-const { v4:uuidV4 } = require('uuid')
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const { ExpressPeerServer } = require('peer');
+const { v4: uuidV4 } = require('uuid');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
 const port = process.env.PORT || 3000;
-console.log('listening on port',port)
-app.set('view engine','ejs')
-app.use(express.static('.'))
-app.use('/peerjs', ExpressPeerServer(server, options));
-app.get('/', (req,res) =>{
-    res.redirect(`/${uuidV4()}`)
-})
+console.log('listening on port', port);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+app.get('/:room/views/*', (req, res) => {
+  const assetPath = req.params[0] || '';
+  res.sendFile(path.join(__dirname, 'views', assetPath), err => {
+    if (err) res.status(err.status || 404).end();
+  });
+});
+
+app.use('/peerjs', ExpressPeerServer(server, { debug: false }));
+
+app.get('/', (_req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get('/:room', (req,res) => {
-    res.render('room', {roomId: req.params.room})
+  res.render('room', {roomId: req.params.room})
 })
 
 io.on('connection', socket => {
-    socket.on('join-room',(roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected',userId)
-        console.log(roomId,userId)
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
+  socket.on('join-room',(roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected',userId)
+    console.log(roomId,userId)
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
     })
-    socket.on('newDoodle',(roomId, data) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('newDoodle',data)
-    })
+  })
+  socket.on('newDoodle',(roomId, data) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('newDoodle',data)
+  })
 })
 
 server.listen(port)
